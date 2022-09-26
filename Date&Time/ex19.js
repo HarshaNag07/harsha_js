53af7c079bbf44d2f0f271671dbb672d7e07314c
HarshaNag07 <harshanag92@gmail.com>
2022-09-26 12:36:57 +0530

    first sample

---
+++ Date&Time/ex19.js
@@ -0,0 +1,36 @@
+var birthdayWishes=[
+
+    {
+
+        title:`For many people, the word friend is just a sequence of letters. For me, it is the source of happiness and strength because of you.
+        Happy Birthday, buddy!
+        `
+    },
+
+    {
+        title:`Hope your Birthday is as special as you are.
+        May all of your dreams come true.
+        Thanks for being such a great friend.
+        Happy Birthday to my friend!`
+    },
+    {
+        title:`Not everyone is as lucky as me to be blessed with a friend like you. Thank you so much for coming into my life and standing by my side through thick and thin. I hope you get all that you truly deserve. Happy Birthday to you.`
+    },
+    {
+        title:`Best friends are hard to come by. That’s why I wanted to let you know just how much your friendship means to me on this special day. Happy Birthday my friend!`
+    },
+    {
+        title:`oday is your birthday! May you receive even more blessings than you ask for, and may all good things continue to come your way. Happy birthday!`
+    },
+    {
+        title:`shing you an awesome day, with good luck on your way; stay blessed and happy this day, my dear friend, on your birthday today.`
+    },
+    {
+        title:`Angels are few, and so are you. And that is what makes you so special. Many Many Happy Returns of the day. Have an amazing day and an even more amazing year ahead.`
+    },
+    {
+        title:`mile as much you can because seeing your smiling face makes my day. Wishing you a very happy birthday with lots of love. Enjoy the day!`
+    }
+
+
+]
---
+++ loops/forof2.js
@@ -0,0 +1,36 @@
+
+    var emp=[
+    {
+        id:1,
+        name:'nag',
+        email:'nag@123',
+        phone:9550449488,
+    },
+    {
+        id:2,
+        name:'jittu',
+        email:'jittu@123',
+        phone:9550449488,
+    },
+    {
+        id:3,
+        name:'bheem',
+        email:'bheem@123',
+        phone:9550449488,
+    },
+    {
+        id:4,
+        name:'ram',
+        email:'ram@123',
+        phone:9550449488,
+    },
+    {
+        id:5,
+        name:'cbn',
+        email:'cbn@123',
+        phone:9550449488,
+    },
+]
+   
+
+
